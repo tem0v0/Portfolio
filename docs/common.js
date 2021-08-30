e372c574559ad2ad1ef5a051e333988b7edebb27
tem0v0 <merbina@naver.com>
2021-08-31 01:54:10 +0900

    포트폴리오 탭메뉴 js 약간 간소화

--- docs/common.js
+++ docs/common.js
@@ -4,39 +4,31 @@ new fullpage('#fullpage', {
 });
 
 function tabMenu() {
-  function hide() {
+  function hideShow(no) {
     $('.tab-menu .pf').removeClass('active');
     $('.tab-menu .body .pf').hide();
-  }
-
-  function show(no) {
     $('.tab-menu .pf-'+no).addClass('active');
     $('.tab-menu .pf-'+no).fadeIn(1800);
   }
 
   $('.tab-menu > .head .pf-1').click(function() {
-    hide();
-    show(1);
+    hideShow(1);
   });
 
   $('.tab-menu > .head .pf-2').click(function() {
-    hide();
-    show(2);
+    hideShow(2);
   });
 
   $('.tab-menu > .head .pf-3').click(function() {
-    hide();
-    show(3);
+    hideShow(3);
   });
 
   $('.tab-menu > .head .pf-4').click(function() {
-    hide();
-    show(4);
+    hideShow(4);
   });
 
   $('.tab-menu > .head .pf-5').click(function() {
-    hide();
-    show(5);
+    hideShow(5);
   });
 }
 
