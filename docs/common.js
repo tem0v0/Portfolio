4efbca66d891f355480c34e0364f72c260fc73a8
tem0v0 <merbina@naver.com>
2021-08-31 01:50:21 +0900

    포트폴리오 탭메뉴 js 추가

--- docs/common.js
+++ docs/common.js
@@ -2,3 +2,42 @@ new fullpage('#fullpage', {
   sectionsColor: ['#202020', '#202020', '#202020', '#202020'],
   menu: ".menu-bar .menu"
 });
+
+function tabMenu() {
+  function hide() {
+    $('.tab-menu .pf').removeClass('active');
+    $('.tab-menu .body .pf').hide();
+  }
+
+  function show(no) {
+    $('.tab-menu .pf-'+no).addClass('active');
+    $('.tab-menu .pf-'+no).fadeIn(1800);
+  }
+
+  $('.tab-menu > .head .pf-1').click(function() {
+    hide();
+    show(1);
+  });
+
+  $('.tab-menu > .head .pf-2').click(function() {
+    hide();
+    show(2);
+  });
+
+  $('.tab-menu > .head .pf-3').click(function() {
+    hide();
+    show(3);
+  });
+
+  $('.tab-menu > .head .pf-4').click(function() {
+    hide();
+    show(4);
+  });
+
+  $('.tab-menu > .head .pf-5').click(function() {
+    hide();
+    show(5);
+  });
+}
+
+tabMenu();
