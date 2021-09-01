e352fdc6521b8958ab180929c0fe06b5d37767e6
tem0v0 <merbina@naver.com>
2021-09-01 18:45:11 +0900

    텍스트 배경에 마우스 따라서 움직이는 js 추가

--- docs/common.js
+++ docs/common.js
@@ -6,44 +6,48 @@ new fullpage('#fullpage', {
 function tabMenu() {
   function hideShow(no) {
     $('.tab-menu .pf').removeClass('active');
-    $('.tab-menu .pf-'+no).addClass('active');
+    $('.tab-menu .pf-' + no).addClass('active');
   }
 
-  $('.tab-menu > .head .pf-1').click(function() {
+  $('.tab-menu > .head .pf-1').click(function () {
     hideShow(1);
   });
 
-  $('.tab-menu > .head .pf-2').click(function() {
+  $('.tab-menu > .head .pf-2').click(function () {
     hideShow(2);
   });
 
-  $('.tab-menu > .head .pf-3').click(function() {
+  $('.tab-menu > .head .pf-3').click(function () {
     hideShow(3);
   });
 
-  $('.tab-menu > .head .pf-4').click(function() {
+  $('.tab-menu > .head .pf-4').click(function () {
     hideShow(4);
   });
 
-  $('.tab-menu > .head .pf-5').click(function() {
+  $('.tab-menu > .head .pf-5').click(function () {
     hideShow(5);
   });
 }
 
 tabMenu();
 
-$("body").mousemove(function(e) {
-  parallaxIt(e, ".text-bg", 20);
-  parallaxIt(e, "body", 0);
-});
-
-function parallaxIt(e, target, movement) {
-  var $this = $("body");
-  var relX = e.pageX - $this.offset().left;
-  var relY = e.pageY - $this.offset().top;
-
-  TweenMax.to(target, 1, {
-    x: (relX - $this.width() / 2) / $this.width() * movement,
-    y: (relY - $this.height() / 2) / $this.height() * movement
+function textBg_move() {
+  $("body").mousemove(function (e) {
+    parallaxIt(e, ".text-bg", 20);
+    parallaxIt(e, "body", 0);
   });
+
+  function parallaxIt(e, target, movement) {
+    var $this = $("body");
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+      x: (relX - $this.width() / 2) / $this.width() * movement,
+      y: (relY - $this.height() / 2) / $this.height() * movement
+    });
+  }
 }
+
+textBg_move();
