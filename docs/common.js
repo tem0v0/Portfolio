e6a097f9623c04ee4f27aed1ffa387c30f42e8b1
tem0v0 <merbina@naver.com>
2021-09-01 18:43:41 +0900

    텍스트 배경에 마우스 따라서 움직이는 js 추가

--- docs/common.js
+++ docs/common.js
@@ -31,3 +31,19 @@ function tabMenu() {
 }
 
 tabMenu();
+
+$("body").mousemove(function(e) {
+  parallaxIt(e, ".text-bg", 20);
+  parallaxIt(e, "body", 0);
+});
+
+function parallaxIt(e, target, movement) {
+  var $this = $("body");
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width() / 2) / $this.width() * movement,
+    y: (relY - $this.height() / 2) / $this.height() * movement
+  });
+}
